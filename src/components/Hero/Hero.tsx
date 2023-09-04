b0e4aab60b4ad495a6d178773cb9771f4fe35e20
Bart Krakowski <bartlomiej@krakowski.dev>
2023-09-04 19:45:32 +0200

    chore: init

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { FC, Suspense, useRef } from "react";
+import { FC, Suspense, useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 
 const View = dynamic(() => import('@/components/View').then((mod) => mod.View), { ssr: false });
@@ -6,15 +6,43 @@ import David from "./components/David";
 import Background from "./components/Background";
 import Pennant from "./components/Pennant";
 import styled from "@emotion/styled";
-import { Physics } from "@react-three/cannon";
+import { Physics } from "@react-three/cannon"
 
 const Hero: FC = () => {
   const cloth = useRef(null);
+  const handleMouseMove = (event) => {
+    const x = (event.clientX - window.innerWidth / 2) / window.innerWidth;
+    const y = -(event.clientY - window.innerHeight / 2) / window.innerHeight;
+  
+    // Sprawdź, czy cloth.current jest zdefiniowane i ma właściwość geometry
+    if (cloth.current && cloth.current.geometry) {
+      const geometry = cloth.current.geometry;
+  
+      // Iteruj przez wierzchołki geometrii i aktualizuj ich pozycję
+      geometry.vertices.forEach((vertex) => {
+        vertex.x += x * 6;
+        vertex.y += y * 6;
+      });
+  
+      // Odśwież geometrię i oblicz normale wierzchołków
+      geometry.verticesNeedUpdate = true;
+      geometry.computeVertexNormals();
+    }
+  };
+  
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+  
   return (
     <Wrapper>
+      <directionalLight position={[1, 1, 1]} />
       <Suspense fallback={null}>
         <Physics iterations={10} gravity={[0, -20, 0]}>
-          <Pennant ref={cloth} width={4} height={4} resolutionX={16} resolutionY={16} />
+          <Pennant ref={cloth} />
         </Physics>
         {/* <David />
         <Background /> */}
--- src/components/Hero/components/Pennant/Pennant.tsx
+++ src/components/Hero/components/Pennant/Pennant.tsx
@@ -1,16 +1,111 @@
-import React, { useRef } from "react";
+import { MutableRefObject, RefObject, createRef, forwardRef, memo, useEffect, useRef, useState } from "react";
 import { useTexture } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { DoubleSide, Vector3, type Mesh } from "three";
-import { type Triplet, usePlane, useSphere } from "@react-three/cannon";
+import { DoubleSide, Vector3, type Mesh, Object3D } from "three";
+import { type Triplet, usePlane, useSphere, useParticle, PublicApi, useDistanceConstraint } from "@react-three/cannon";
 
 import texturePng from "./assets/pennant.png";
 import alphaMapPng from "./assets/alphaMap.png";
 
-const Pennant = () => {
+const WIDTH = 4
+const HEIGHT = 4
+const RESOLUTIONX = 16
+const RESOLUTIONY = 16
+
+type Particle = {
+  particle: React.MutableRefObject<Object3D>
+  api: PublicApi
+}
+
+interface ParticleProps {
+  mass: number;
+  position: Triplet;
+}
+const Particle = memo(
+  forwardRef<{
+    particle: MutableRefObject<Object3D>;
+    api: PublicApi;
+  }, ParticleProps>(({ mass, position }, ref) => {
+    let [particle, api] = useParticle(() => ({
+      mass,
+      position,
+      // args: [0.3],
+      linearDamping: 0.2
+    }))
+
+    if (ref && particle.current && typeof ref === 'object') {
+      ref.current = { particle, api };
+    }
+
+    return null
+  })
+)
+
+interface StitchProps {
+  p1: RefObject<Particle>;
+  p2: RefObject<Particle>;
+  distance: number;
+}
+const Stitch = memo<StitchProps>(({ p1, p2, distance = 0.1 }) => {
+  useDistanceConstraint(p1.current.particle, p2.current.particle, {
+    distance
+  })
+
+  return null
+})
+Stitch.displayName = "Stitch";
+
+const Pennant = forwardRef<any, any>((props, ref) => {
   const pennantTexture = useTexture(texturePng.src);
   const alphaMap = useTexture(alphaMapPng.src);
   const { viewport } = useThree();
+  const box = useRef<Mesh>(null)
+  const [readyForStitches, setReadyForStitches] = useState(false)
+
+  const particles = useRef(Array.from({ length: RESOLUTIONY }, () => Array.from<RefObject<Particle>, RefObject<Particle>>({ length: RESOLUTIONX }, createRef)))
+
+  useEffect(() => {
+    setReadyForStitches(true)
+  }, [])
+
+  useFrame(() => {
+    if (particles.current[0][0]) {
+      const geom = box.current.geometry;
+      const positions = geom.getAttribute('position');
+      const positionArray = positions.array;
+  
+      for (let vi = 0; vi < positionArray.length; vi += 3) {
+        let x = vi / 3 % RESOLUTIONX;
+        let y = Math.floor(vi / 3 / RESOLUTIONX);
+        const particle = particles.current[y][x].current?.particle.current.position;
+        
+        if (particle) {
+        positionArray[vi] = particle.x;
+        positionArray[vi + 1] = particle.y;
+        positionArray[vi + 2] = particle.z;
+        }
+      }
+  
+      positions.needsUpdate = true;
+      geom.computeVertexNormals();
+    }
+  });
+
+  const distanceX = WIDTH / RESOLUTIONX
+  const distanceY = HEIGHT / RESOLUTIONY
+  const distanceDiagonal = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+  const setPosition = (x = 0, y = 0, z = 0) => {
+    particles.current[0].forEach((p, i) => {
+      if (i < 2 || i > particles.current[0].length - 3)
+        p.current.api.position.set((-distanceX * RESOLUTIONX) / 2 + x + distanceX * i, y, z)
+    })
+  }
+
+  if (ref && typeof ref === 'object') {
+    ref.current = {
+      setPosition
+    };
+  }
 
   const halfScreenWidth = viewport.width / 2 - 0.4;
   const halfScreenHeight = viewport.height / 2 - 0.51;
@@ -21,30 +116,55 @@ const Pennant = () => {
     position: [-halfScreenWidth, halfScreenHeight, 0.1],
   }));
 
-  const windForce = useRef<Triplet>([0.0, 0.0, 0.05]);
-
-  useFrame(() => {
-    // Aplikuj siłę wiatru do fladzy
-    api.applyForce(windForce.current, [0, 0, 0]);
-  });
-
-  const pennantPosition = useRef<Vector3>(new Vector3(0, 0, 0.1));
-
-  useFrame(() => {
-    // Aktualizuj pozycję fladzy
-    pennantRef.current.position.copy(pennantPosition.current);
-  });
+  // <mesh
+  //   ref={pennantRef}
+  //   castShadow
+  //   receiveShadow
+  // >
+  //   <planeGeometry args={[0.5, 1]} />
+  //   <meshBasicMaterial side={DoubleSide} map={pennantTexture} alphaMap={alphaMap} transparent />
+  // </mesh>
 
   return (
-    <mesh
-      ref={pennantRef}
-      castShadow
-      receiveShadow
-    >
-      <planeGeometry args={[0.5, 1]} />
-      <meshBasicMaterial side={DoubleSide} map={pennantTexture} alphaMap={alphaMap} transparent />
-    </mesh>
+    <group>
+      <mesh ref={box}>
+        <planeGeometry args={[WIDTH, HEIGHT, RESOLUTIONX - 1, RESOLUTIONY - 1]} />
+        <meshStandardMaterial color={'red'} side={DoubleSide} />
+      </mesh>
+      {particles.current.map((y, yi) =>
+        y.map((x, xi) => (
+          <Particle
+            ref={x}
+            mass={yi === 0 && (xi < 2 || xi > RESOLUTIONX - 3) ? 0 : (1 / WIDTH) * HEIGHT}
+            key={yi + '-' + xi}
+            position={[(xi * WIDTH) / RESOLUTIONX, (yi * -HEIGHT) / RESOLUTIONX + 2, 0]}
+          />
+        ))
+      )}
+      {readyForStitches &&
+        particles.current.map((y, yi) =>
+          y.map((x, xi) => {
+            return (
+              <>
+                {xi < RESOLUTIONX - 1 && (
+                  <Stitch key={yi + '-' + xi + 'x'} p1={x} p2={particles.current[yi][xi + 1]} distance={distanceX} />
+                )}
+                {yi < RESOLUTIONY - 1 && (
+                  <Stitch key={yi + '-' + xi + 'y'} p1={x} p2={particles.current[yi + 1][xi]} distance={distanceY} />
+                )}
+                {yi < RESOLUTIONY - 1 && xi < RESOLUTIONX - 1 && (
+                  <Stitch key={yi + '-' + xi + 's1'} p1={x} p2={particles.current[yi + 1][xi + 1]} distance={distanceDiagonal} />
+                )}
+                {yi > 0 && xi < RESOLUTIONX - 1 && (
+                  <Stitch key={yi + '-' + xi + 's2'} p1={x} p2={particles.current[yi - 1][xi + 1]} distance={distanceDiagonal} />
+                )}
+              </>
+            )
+          })
+        )}
+    </group>
   );
-};
+});
 
+Pennant.displayName = "Pennant";
 export default Pennant;
